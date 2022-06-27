d1fdfe89fdfe181e579786f903b15cb91a94baca
secret-dev87 <dev.bruce.87@gmail.com>
2022-06-27 15:55:41 +0300

    User Db model added & Migration created & Db updated

--- EcommerceBlazor/Server/Data/DataContext.cs
+++ EcommerceBlazor/Server/Data/DataContext.cs
@@ -253,6 +253,8 @@
         public DbSet<Category> Categories { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
         public DbSet<ProductVariant> ProductVariants { get; set; }
+        public DbSet<User> Users { get; set; }
+
 
     }
 }
