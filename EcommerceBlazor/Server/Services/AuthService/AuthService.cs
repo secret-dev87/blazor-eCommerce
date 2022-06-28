b323b2e59ef4aa4af6342c688f73c40d27f33c1f
secret-dev87 <dev.bruce.87@gmail.com>
2022-06-28 21:49:41 +0300

    Login added to Server Services and Controller

--- EcommerceBlazor/Server/Services/AuthService/AuthService.cs
+++ EcommerceBlazor/Server/Services/AuthService/AuthService.cs
@@ -11,13 +11,36 @@ namespace EcommerceBlazor.Server.Services.AuthService
             _context = context;
         }
 
+        public async Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            var response = new ServiceResponse<string>();
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Email.ToLower().Equals(email.ToLower()));
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "User not found.";
+            }
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "Wrong password.";
+            }
+            else
+            {
+                response.Data = "token";
+            }
+
+            return response;
+        }
+
         public async Task<ServiceResponse<int>> Register(User user, string password)
         {
             //check if user exists
-            if(await UserExists(user.Email))
+            if (await UserExists(user.Email))
             {
                 return new ServiceResponse<int>
-                { 
+                {
                     Success = false,
                     Message = "User already exists."
                 };
@@ -40,7 +63,7 @@ namespace EcommerceBlazor.Server.Services.AuthService
         //checks if user exists 
         public async Task<bool> UserExists(string email)
         {
-            if(await _context.Users.AnyAsync(user => user.Email.ToLower()
+            if (await _context.Users.AnyAsync(user => user.Email.ToLower()
                 .Equals(email.ToLower())))
             {
                 return true;
@@ -51,11 +74,22 @@ namespace EcommerceBlazor.Server.Services.AuthService
         //encrypt users password using special cryptography method
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using(var hmac = new HMACSHA512())
+            using (var hmac = new HMACSHA512())
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.
                     ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash =
+                    hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                //compare the two variables byte by byte automatically
+                return computedHash.SequenceEqual(passwordHash);
             }
         }
     }
